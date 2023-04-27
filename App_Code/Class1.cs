fe2f3aa80202f2f26366c44017ed382cb13dc8ed
Priyansh Modi <76965511+LeGi0N09@users.noreply.github.com>
2023-04-27 13:05:32 +0530

    Add files via upload

---
+++ App_Code/Class1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+   
+    SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\sahar\OneDrive\Desktop\Final_Project\Final_Project\App_Data\Database.mdf;Integrated Security=True");
+    SqlCommand cmd = new SqlCommand();
+    SqlCommand cmd1 = new SqlCommand();
+    SqlCommand cmd2 = new SqlCommand();
+    SqlDataReader rdr;
+    public int flag = 0;
+    public int flag2 = 0;
+    public int flag3 = 0;
+    public Class1()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+        cmd.Connection = con;
+        cmd1.Connection = con;
+        cmd2.Connection = con;
+    }
+    public string[] log(string email, string Password  )
+    {
+        string qry = "select * from login where email=@p1 and Password=@p2";
+        cmd.CommandText = qry;   
+        cmd.Parameters.Clear();
+        cmd.Parameters.AddWithValue("@p1", email);
+        cmd.Parameters.AddWithValue("@p2", Password);
+        
+
+        con.Open();
+        rdr = cmd.ExecuteReader();
+
+        string[] type = new string[3];
+
+        if (rdr.Read())
+        {
+            type[0] = Convert.ToString(rdr["role"]);
+            type[1] = Convert.ToString(rdr["username"]);
+            type[2] = Convert.ToString(rdr["status"]);
+        }
+        con.Close();
+        return type;
+    }
+
+    public void ins_user(string p, string p2, string p3, string p4, string p5, string p6, string p7, string p8, string p9,string p10,string p11,string p12,string p13)
+    {
+
+        SqlCommand cmd = new SqlCommand("insert into customer_signup values(@firstname,@lastname,@age,@dob,@gender,@email,@number,@altnumber,@address,@profile,@username,@password,@status )", con);
+        con.Open();
+        cmd.Parameters.AddWithValue("@firstname", p);
+        cmd.Parameters.AddWithValue("@lastname", p2);
+        cmd.Parameters.AddWithValue("@age", p3);
+        cmd.Parameters.AddWithValue("@dob", p4);
+        cmd.Parameters.AddWithValue("@gender", p5);
+        cmd.Parameters.AddWithValue("@email", p6);
+        cmd.Parameters.AddWithValue("@number", p7);
+        cmd.Parameters.AddWithValue("@altnumber", p8);
+        cmd.Parameters.AddWithValue("@address", p9);
+        cmd.Parameters.AddWithValue("@profile", p10);
+
+        cmd.Parameters.AddWithValue("@username", p11);
+        cmd.Parameters.AddWithValue("@password", p12);
+        cmd.Parameters.AddWithValue("@status", p13);
+
+        flag= 1;
+        cmd.ExecuteNonQuery();
+        
+
+        //-------------------------------------------------------------------------------
+
+
+        SqlCommand cmd1 = new SqlCommand("insert into login values(@user,@email,@username,@password,@status)",con);
+        cmd.Parameters.Clear();   
+        
+        cmd1.Parameters.AddWithValue("@user", "user");
+        cmd1.Parameters.AddWithValue("@email", p6);
+        cmd1.Parameters.AddWithValue("@username",p11);
+        cmd1.Parameters.AddWithValue("@password", p12);
+        cmd1.Parameters.AddWithValue("@status", p13);
+
+
+        cmd1.ExecuteNonQuery();
+        con.Close();
+     
+    }
+
+    public void ins_admin(string p,string p2,string p3)
+    {
+        SqlCommand cmd2 = new SqlCommand("insert into login values(@admin,@email,@username,@password)", con);
+        con.Open();
+        cmd2.Parameters.Clear(); 
+        cmd2.Parameters.AddWithValue("@admin", "admin");
+        cmd2.Parameters.AddWithValue("@email", p);
+        cmd2.Parameters.AddWithValue("@username", p2);
+        cmd2.Parameters.AddWithValue("@password", p3);
+      
+
+        cmd2.ExecuteNonQuery();
+        con.Close();
+    }
+    public void EmailExist(string email)
+    {
+        con.Open();
+        SqlCommand cmd = new SqlCommand("select * from login where email =@email", con);
+        cmd.Parameters.AddWithValue("@email", email);
+        SqlDataAdapter sda = new SqlDataAdapter(cmd);
+        DataSet ds = new DataSet();
+        sda.Fill(ds);
+        if (ds.Tables[0].Rows.Count > 0)
+        {
+            flag2 = 1;
+        }
+        else
+        {
+            flag2 = 0;
+        }
+        con.Close();
+    }
+    public void UserExist(string username)
+    {
+        con.Open();
+        SqlCommand cmd = new SqlCommand("select * from login where username =@username", con);
+        cmd.Parameters.AddWithValue("@username", username);
+        SqlDataAdapter sda = new SqlDataAdapter(cmd);
+        DataSet ds = new DataSet();
+        sda.Fill(ds);
+        if (ds.Tables[0].Rows.Count > 0)
+        {
+            flag3 = 1;
+        }
+        else
+        {
+            flag3 = 0;
+        }
+        con.Close();
+    }
+}
---
+++ App_Code/Class2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+/// <summary>
+/// Summary description for Class2
+/// </summary>
+public class Class2
+{
+    SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\sahar\OneDrive\Desktop\Final_Project\Final_Project\App_Data\Database.mdf;Integrated Security=True");
+    SqlCommand cmd = new SqlCommand();
+    SqlCommand cmd1 = new SqlCommand();
+
+    
+    public int flag = 0;
+	public Class2()
+	{
+        cmd.Connection = con;
+        cmd1.Connection = con;
+	}
+    public void ins_vendor(string p, string p2, string p3, string p4, string p5, string p6, string p7, string p8, string p9, string p10, string p11, string p12, string p13, string p14, string p15, string p16, string p17, string p18, string p19, string p20, string p21, string p22, string p23,string p24,string p25,string p26)
+    {
+
+        SqlCommand cmd = new SqlCommand("insert into vendor_signup values(@name,@age,@dob,@gender,@companyname,@category,@subcategory,@services,@materials,@staff,@charge,@paymenttype,@deposit,@notice,@address,@city,@pincode,@photo,@idtype,@idphoto,@email,@phone,@alterphone,@username,@password,@status )", con);
+        con.Open();
+        cmd.Parameters.AddWithValue("@name", p);
+        cmd.Parameters.AddWithValue("@age", p2);
+        cmd.Parameters.AddWithValue("@dob", p3);
+        cmd.Parameters.AddWithValue("@gender", p4);
+        cmd.Parameters.AddWithValue("@companyname", p5);
+        cmd.Parameters.AddWithValue("@category", p6);
+        cmd.Parameters.AddWithValue("@subcategory", p7);
+        cmd.Parameters.AddWithValue("@services", p8);
+        cmd.Parameters.AddWithValue("@materials", p9);
+        cmd.Parameters.AddWithValue("@staff", p10);
+        cmd.Parameters.AddWithValue("@charge", p11);
+        cmd.Parameters.AddWithValue("@paymenttype", p12);
+        cmd.Parameters.AddWithValue("@deposit", p13);
+        cmd.Parameters.AddWithValue("@notice", p14);
+        cmd.Parameters.AddWithValue("@address", p15);
+        cmd.Parameters.AddWithValue("@city", p16);
+        cmd.Parameters.AddWithValue("@pincode", p17);
+        cmd.Parameters.AddWithValue("@photo", p18);
+        cmd.Parameters.AddWithValue("@idtype", p19);
+        cmd.Parameters.AddWithValue("@idphoto", p20);
+        cmd.Parameters.AddWithValue("@email", p21);
+        cmd.Parameters.AddWithValue("@phone", p22);
+        cmd.Parameters.AddWithValue("@alterphone", p23);
+        cmd.Parameters.AddWithValue("@username", p24);
+        cmd.Parameters.AddWithValue("@password", p25);
+        cmd.Parameters.AddWithValue("@status", p26);
+
+        flag = 1;
+        cmd.ExecuteNonQuery();
+
+
+        //-------------------------------------------------------------------------------
+        cmd.CommandText = "select max(id) from vendor_signup";
+        string username = Convert.ToString(cmd.ExecuteScalar());
+
+
+        SqlCommand cmd1 = new SqlCommand("insert into login values(@user,@email,@username,@password,@status)", con);
+        cmd.Parameters.Clear();
+
+        cmd1.Parameters.AddWithValue("@user", "vendor");
+        cmd1.Parameters.AddWithValue("@email", p21);
+        cmd1.Parameters.AddWithValue("@username", p24);
+        cmd1.Parameters.AddWithValue("@password", p25);
+        cmd1.Parameters.AddWithValue("@status", p26);
+
+
+        cmd1.ExecuteNonQuery();
+        con.Close();
+
+    }
+}
